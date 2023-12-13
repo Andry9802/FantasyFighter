82e734f12fa6bc9da187caa43bc6e9be1aee934c
Andrea <andrea_reggina@hotmail.it>
2023-12-13 13:06:05 +0100

    modifiche

---
+++ FantasyFighter/Characters/RatKing.cs
@@ -0,0 +1,20 @@
+﻿namespace FantasyFighter
+{
+    public class RatKing : Character, ICanDefend, ICanAttack
+    {
+        public RatKing(string name) : base(name, health: 120, attackPoints: 25, defensePoints: 10)
+        {
+
+        }
+
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Defend()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- FantasyFighter/Dragon.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FantasyFighter
-{
-    public class Dragon : Character
-    {
-        public Dragon(string name, int health, int attackPoints, int defensePoints) : base(name, health, attackPoints, defensePoints)
-        {
-        }
-    }
-}
